0f2f65acdb8272e9889b9722d4b4500e00a94a5a
Yoga Paripurna <63484583+YogaParipurna@users.noreply.github.com>
2021-04-10 14:51:15 +0700

    first commit

---
+++ praktikum_jquery.js
@@ -0,0 +1,44 @@
+document.getElementById('nama').innerHTML ='Yoga Paripurna';
+
+$('#nim').html('20190140039');
+
+$(function(){
+    console.log("Hell");
+});
+
+$("#hide").on("click", function(){
+    $("#nama").hide();
+});
+
+$("#show").on("click", function(){
+    $("#nama").show();
+});
+
+$("#toggle").on("click", function(){
+    $("#nama").toggle();
+});
+
+$("#fadeIn").on("click", function(){
+    $("#tanggal").fadeIn();
+});
+
+$("#fadeOut").on("click", function(){
+    $("#tanggal").fadeOut();
+});
+
+$("#fadeToggle").on("click", function(){
+    $("#tanggal").fadeToggle();
+});
+
+$("#slideUp").on("click", function(){
+    $("#img").slideUp();
+});
+
+$("#slideDown").on("click", function(){
+    $("#img").slideDown();
+});
+
+$("#slideToggle").on("click", function(){
+    $("#img").slideToggle();
+});
+
